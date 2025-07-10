a15744eccc7472e3eac170c7cd574ad692e00a14
faith rono <faithrono132@gmail.com>
2025-07-10 15:06:26 +0300

    refactor(batch_processing): return filtered users over age 25

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -20,7 +20,10 @@ def stream_users_in_batches(batch_size):
     connection.close()
 
 def batch_processing(batch_size):
+    processed_users = []
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user['age'] > 25:
                 print(user)
+                processed_users.append(user)
+    return processed_users
