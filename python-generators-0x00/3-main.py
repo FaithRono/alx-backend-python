31ed1b565163069b5f8b06aa20d700fa35028d07
faith rono <faithrono132@gmail.com>
2025-07-10 15:17:52 +0300

    add lazy_pagination generator to fetch paginated user data efficiently using LIMIT and OFFSET

--- python-generators-0x00/3-main.py
+++ python-generators-0x00/3-main.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 import sys
-processing = __import__('1-batch_processing')
+lazy_paginator = __import__('2-lazy_paginate').lazy_pagination
 
-##### print processed users in a batch of 50
+
 try:
-    processing.batch_processing(50)
+    for page in lazy_paginator(100):
+        for user in page:
+            print(user)
+
 except BrokenPipeError:
     sys.stderr.close()
-
-(venv) faithokoth@Faiths-MacBook-Pro python-generators-0x00 % ./2-main.py | head -n 5 
+(venv) faithokoth@Faiths-MacBook-Pro python-generators-0x00  % python 3-main.py | head -n 7
 
 {'user_id': '00234e50-34eb-4ce2-94ec-26e3fa749796', 'name': 'Dan Altenwerth Jr.', 'email': 'Molly59@gmail.com', 'age': 67}
 
@@ -16,8 +18,11 @@ except BrokenPipeError:
 
 {'user_id': '006e1f7f-90c2-45ad-8c1d-1275d594cc88', 'name': 'Daniel Fahey IV', 'email': 'Delia.Lesch11@hotmail.com', 'age': 49}
 
+{'user_id': '00af05c9-0a86-419e-8c2d-5fb7e899ae1c', 'name': 'Ronnie Bechtelar', 'email': 'Sandra19@yahoo.com', 'age': 22}
+
 {'user_id': '00cc08cc-62f4-4da1-b8e4-f5d9ef5dbbd4', 'name': 'Alma Bechtelar', 'email': 'Shelly_Balistreri22@hotmail.com', 'age': 102}
 
 {'user_id': '01187f09-72be-4924-8a2d-150645dcadad', 'name': 'Jonathon Jones', 'email': 'Jody.Quigley-Ziemann33@yahoo.com', 'age': 116}
 
+{'user_id': '01ab6c5d-7ae2-4968-991a-d63e93d8d025', 'name': 'Forrest Heaney', 'email': 'Albert51@hotmail.com', 'age': 104}
 (venv) faithokoth@Faiths-MacBook-Pro python-generators-0x00 % 
